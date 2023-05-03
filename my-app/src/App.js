a0c54bbe51e640259c63497c3e97404f711275d4
ikram <ikranidil@hotmail.co.uk>
2023-05-03 20:20:08 +0100

    todo input refreshing

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
-import TodoForm from './Components/TodoForm';
+// import TodoForm from './Components/TodoForm';
+import TodoList from './Components/TodoList';
 
 function App() {
   return (
     <div className="todo-app">
-   <TodoForm/>
+   <TodoList/>
     </div>
   );
 }
